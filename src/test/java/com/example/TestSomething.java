62aeaaf006edb751bdad5796fc1948691bb00baf
collerty <nhlsviatoslav@gmail.com>
2025-03-07 11:32:11 +0100

    added mvn, so you can run 'mvn test'

---
+++ src/test/java/com/example/TestSomething.java
@@ -0,0 +1,23 @@
+package test.java.com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSomething {
+
+    @BeforeEach
+    public void setUp() {
+        // setup code here
+    }
+
+    @Test
+    public void testPass() {
+        assertTrue(true);
+    }
+
+//    @Test
+//    public void testFail() {
+//        fail();
+//    }
+}
